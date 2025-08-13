c069a2bb37a33bda95e2f69288f86c423fdf76ea
MOhammad Ebrahmyan <mohammadebrahimiyan34@gmail.com>
2025-08-13 15:44:54 +0330

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Architecture-Web-Portfolio/'
+  // base: '/Architecture-Web-Portfolio/'f
 });
